446518dc701d86513dae2774b89bdceaddaafd61
chfw <wangc_2011@hotmail.com>
2018-11-18 10:33:33 +0000

    :tractor: relocate engine_handlebars.py and :fire: getting rid of other files

--- __init__.py
+++
@@ -1,4 +0,0 @@
-from moban._version import __version__
-from moban._version import __author__
-
-__all__ = ("__author__", "__version__")
--- __main__.py
+++
@@ -1,4 +0,0 @@
-from moban.main import main
-
-if __name__ == "__main__":
-    main()
