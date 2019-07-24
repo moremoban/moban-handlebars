51d758d193e46b91c67fedf2c7693e70dbd0654c
chfw <wangc_2011@hotmail.com>
2019-07-24 19:00:47 +0100

    :sparkles: migrated to python file system 2

--- moban_handlebars/engine.py
+++ moban_handlebars/engine.py
@@ -1,8 +1,10 @@
 import sys
-import codecs
 
 import moban.utils as utils
 from pybars import Compiler
+import fs
+
+PY2 = sys.version_info[0] == 2
 
 
 class EngineHandlebars(object):
@@ -13,17 +15,33 @@ class EngineHandlebars(object):
         actual_file = utils.get_template_path(
             self.template_dirs, template_file
         )
-        with codecs.open(actual_file, "r", encoding="utf-8") as source:
-            hbr_template = Compiler().compile(source.read())
+        content = read_file(actual_file)
+        hbr_template = Compiler().compile(content)
         return hbr_template
 
     def get_template_from_string(self, string):
-        if sys.version_info < (3, 0):
+        if PY2:
             # Python 2 strings are not unicode by default
-            string = unicode(string)
+            string = to_unicode(string)
         return Compiler().compile(string)
 
     def apply_template(self, template, data, _):
         rendered_content = "".join(template(data))
         rendered_content = rendered_content
         return rendered_content
+
+
+def read_file(path):
+    path = to_unicode(path)
+    dir_name = fs.path.dirname(path)
+    the_file_name = fs.path.basename(path)
+    with fs.open_fs(dir_name) as fs_system:
+        with fs_system.open(the_file_name) as file_handle:
+            return file_handle.read()
+
+
+def to_unicode(path):
+    if PY2:
+        if isinstance(path, unicode) is False:
+            return unicode(path)
+    return path
