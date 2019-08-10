d82f1174db9571bb0a8a9c8d01c886b00db0c7e7
chfw <wangc_2011@hotmail.com>
2019-08-10 17:55:50 +0100

    :green_heart: update test codes

--- tests/test_handlebar_engine.py
+++ tests/test_handlebar_engine.py
@@ -1,7 +1,7 @@
 import os
 
 from nose.tools import eq_
-from moban.plugins import ENGINES, BaseEngine
+from moban.plugins import ENGINES
 from moban_handlebars.engine import EngineHandlebars
 
 
@@ -17,13 +17,13 @@ def test_handlebars_template_not_found():
 
 def test_handlebars_template_type():
     engine = ENGINES.get_engine("hbs", [], "")
-    assert engine.engine_cls == EngineHandlebars
+    assert engine.engine.__class__.__name__ == 'EngineHandlebars'
 
 
 def test_handlebars_file_tests():
     output = "test.txt"
     path = os.path.join("tests", "fixtures", "handlebars_tests")
-    engine = BaseEngine([path], path, EngineHandlebars)
+    engine = ENGINES.get_engine("hbs", [path], path)
     engine.render_to_file("file_tests.template", "file_tests.json", output)
     with open(output, "r") as output_file:
         content = output_file.read()
@@ -35,7 +35,7 @@ def test_handlebars_string_template():
     template_string = "{{test}}"
     output = "test.txt"
     path = os.path.join("tests", "fixtures", "handlebars_tests")
-    engine = BaseEngine([path], path, EngineHandlebars)
+    engine = ENGINES.get_engine("hbs", [path], path)
     engine.render_string_to_file(template_string, "file_tests.json", output)
     with open(output, "r") as output_file:
         content = output_file.read()
