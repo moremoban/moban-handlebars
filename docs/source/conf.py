6aa8df5a10b59b62b2dcad5dbe9654340536b792
chfw <wangc_2011@hotmail.com>
2019-10-12 21:01:03 +0100

    :books: update release tag

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,9 +20,9 @@ project = ''
 copyright = ''
 author = 'Charlie Liu, C.W. et al'
 # The short X.Y version
-version = '0.0.3'
+version = '0.0.4'
 # The full version, including alpha/beta/rc tags
-release = '0.0.3'
+release = '0.0.4'
 
 # -- General configuration ---------------------------------------------------
 
