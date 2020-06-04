c7193a85f7ad20386db949a40884d50deb6a56fe
chfw <wangc_2011@hotmail.com>
2020-06-04 23:04:40 +0100

    :lipstick: update coding style

--- moban_handlebars/engine.py
+++ moban_handlebars/engine.py
@@ -1,3 +1,5 @@
+from typing import Dict
+
 from pybars import Compiler
 from moban.externals import file_system
 
@@ -6,28 +8,28 @@ class EngineHandlebars(object):
     ACTION_IN_PRESENT_CONTINUOUS_TENSE = "Handlebars-ing"
     ACTION_IN_PAST_TENSE = "Handlebarsed"
 
-    def __init__(self, template_fs, options=None):
+    def __init__(self, template_fs, options: Dict = None):
         """
         template_fs is a multfs instance and gives you the power to load
         a template from equiped template directories.
 
-        :param fs.multifs.MultiFS template_fs: a MultiFS instance or a FS instance
+        :param fs.multifs.MultiFS template_fs: a MultiFS/FS instance
         :param options: a dictionary of potential parameters.
                         not used yet.
         """
         self.template_fs = template_fs
 
-    def get_template(self, template_file):
+    def get_template(self, template_file: str):
         template_file = file_system.to_unicode(template_file)
         content = self.template_fs.readtext(template_file)
         hbr_template = Compiler().compile(content)
         return hbr_template
 
-    def get_template_from_string(self, string):
+    def get_template_from_string(self, string: str):
         string = file_system.to_unicode(string)
         return Compiler().compile(string)
 
-    def apply_template(self, template, data, _):
+    def apply_template(self, template, data: Dict, _):
         rendered_content = "".join(template(data))
         rendered_content = rendered_content
         return rendered_content
